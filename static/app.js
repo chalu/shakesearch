486dbbaac5af82a29d4c17db6ade72922efc6e09
Charles Odili <chaluwa@gmail.com>
2023-02-23 12:39:32 -0700

    fix JS load trigger

--- static/app.js
+++ static/app.js
@@ -1,24 +1,30 @@
-const Controller = {
-  search: (ev) => {
-    ev.preventDefault();
-    const form = document.getElementById("form");
-    const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
-      response.json().then((results) => {
-        Controller.updateTable(results);
-      });
-    });
-  },
+const Controller = {};
 
-  updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
-    for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
-    }
-    table.innerHTML = rows;
-  },
+Controller.search = (ev) => {
+  ev.preventDefault();
+  const form = document.getElementById('form');
+  const data = Object.fromEntries(new FormData(form));
+  const response = fetch(`/search?q=${data.query}`).then((response) => {
+    response.json().then((results) => {
+      Controller.updateTable(results);
+    });
+  });
 };
 
-const form = document.getElementById("form");
-form.addEventListener("submit", Controller.search);
+Controller.updateTable = (results) => {
+  const table = document.getElementById('table-body');
+  const rows = [];
+  for (let result of results) {
+    rows.push(`<tr>${result}<tr/>`);
+  }
+  table.innerHTML = rows;
+};
+
+const startApp = () => {
+  const form = document.querySelector('#form');
+  if (form) {
+    form.addEventListener('submit', Controller.search);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', startApp);
