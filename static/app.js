14dfdf4529fd614bd6657a91134ef6d71af40c28
Charles Opute Odili <chaluwa@gmail.com>
2023-02-23 14:25:06 -0700

    Refactor JS (#26)
    * fix JS load trigger
    
    * improve search call
    
    * improve displaying results call
    
    * auto focus search input on page load
    
    * fix results render
    
    * fix code style flags
    
    * trigger another build
    
    * triger another build for Codacy

--- static/app.js
+++ static/app.js
@@ -1,24 +1,58 @@
-const Controller = {
-  search: (ev) => {
-    ev.preventDefault();
-    const form = document.getElementById("form");
-    const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
-      response.json().then((results) => {
-        Controller.updateTable(results);
-      });
-    });
-  },
+const Controller = {};
 
-  updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
-    for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
-    }
-    table.innerHTML = rows;
-  },
+Controller.search = async (evt) => {
+  evt.preventDefault();
+  const form = evt.target;
+  const fData = new FormData(form);
+  const query = fData.get("query");
+  // TODO handle validation
+  // TODO update UI that search has begun
+
+  let results;
+  try {
+    const response = await fetch(`/search?q=${query}`);
+    results = await response.json();
+  } catch (err) {
+    console.warn(err.message);
+    // TODO inform user
+  } finally {
+    // TODO update UI that search has ended
+  }
+
+  if (results) {
+    // TODO expand on this check if needed
+    Controller.displayResults(results);
+  }
 };
 
-const form = document.getElementById("form");
-form.addEventListener("submit", Controller.search);
+// TODO move away from displaying the results with a table
+Controller.displayResults = (results) => {
+  const rows = results.reduce((trs, txt) => {
+    const tr = document.createElement("tr");
+    // TODO If needed, sanitize txt before displaying it
+    // Dev tools shows it has some special chars (\r\n e.t.c)
+    tr.innerText = txt;
+    trs.push(tr);
+    return trs;
+  }, []);
+
+  const tBody = document.querySelector("#table-body");
+  requestAnimationFrame(() => {
+    while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+    }
+    requestAnimationFrame(() => {
+      tBody.append(...rows);
+    });
+  });
+};
+
+const startApp = () => {
+  const form = document.querySelector("#form");
+  if (form) {
+    form.addEventListener("submit", Controller.search);
+    form.querySelector("input[type=text]").focus();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", startApp);
