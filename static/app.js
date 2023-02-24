ad73ada3866465c35586c3d353a37592876a8ae8
Charles Odili <chaluwa@gmail.com>
2023-02-24 15:35:52 -0700

    hint on delayed server response

--- static/app.js
+++ static/app.js
@@ -9,6 +9,59 @@ const ShakeSearch = {
 
 const validationPtrn = /^[a-zA-Z]{3}[ a-zA-Z]*$/;
 
+ShakeSearch.Controller.search = async (evt) => {
+  evt.preventDefault();
+  if (ShakeSearch.State.searching) return;
+
+  const form = evt.target;
+  const fData = new FormData(form);
+  const query = fData.get("query").trim();
+
+  if (!validationPtrn.test(query)) return;
+  if (ShakeSearch.State.prevQry && ShakeSearch.State.prevQry == query) return;
+
+  let results;
+  ShakeSearch.State.prevQry = query;
+  ShakeSearch.Controller.signalSearchStarted();
+  try {
+    ShakeSearch.UI.prepareToHintResponseDelay();
+    const response = await fetch(`/search?q=${query}`);
+    results = await response.json();
+  } catch (err) {
+    ShakeSearch.Controller.showSnackBar("Search failed. Pls try again!");
+    console.warn(err.message);
+  } finally {
+    ShakeSearch.Controller.signalSearchEnded();
+  }
+
+  if (results) {
+    // TODO expand on this check if needed
+    ShakeSearch.Controller.displayResults(results);
+  }
+};
+
+// TODO move away from displaying the results with a table
+ShakeSearch.Controller.displayResults = (results) => {
+  const rows = results.reduce((trs, txt) => {
+    const tr = document.createElement("tr");
+    // TODO If needed, sanitize txt before displaying it
+    // Dev tools shows it has some special chars (\r\n e.t.c)
+    tr.innerText = txt;
+    trs.push(tr);
+    return trs;
+  }, []);
+
+  const tBody = document.querySelector("#table-body");
+  requestAnimationFrame(() => {
+    while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+    }
+    requestAnimationFrame(() => {
+      tBody.append(...rows);
+    });
+  });
+};
+
 ShakeSearch.UI.get = (...selectors) => {
   const nodes = selectors.reduce((gatherer, sel) => {
     gatherer.push(document.querySelector(sel));
@@ -19,19 +72,59 @@ ShakeSearch.UI.get = (...selectors) => {
   return nodes;
 };
 
-ShakeSearch.Controller.showSnackBar = (msg) => {
+ShakeSearch.UI.task = async (todo, opts = {}) => {
+  const { waitUntil = 0, okayToProceed = () => true } = opts;
+
+  if (waitUntil <= 0) {
+    return new Promise((resolve, reject) => {
+      try {
+        const out = okayToProceed() === true ? todo() : undefined;
+        resolve(out);
+      } catch (err) {
+        reject(err.message);
+      }
+    });
+  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        const out = okayToProceed() === true ? todo() : undefined;
+        resolve(out);
+      } catch (err) {
+        reject(err.message);
+      }
+    }, waitUntil);
+  });
+};
+
+/**
+ * Uses a Promise to show a hint after 2s of issuing
+ * a search query, and no response from the server
+ * @returns Promise
+ */
+ShakeSearch.UI.prepareToHintResponseDelay = () => {
+  const tsk = () => ShakeSearch.Controller.showSnackBar("Still busy, pls wait ...");
+  const proceedIf = () => ShakeSearch.State.searching === true;
+
+  return ShakeSearch.UI.task(tsk, {
+    waitUntil: 1500,
+    okayToProceed: proceedIf
+  });
+};
+
+ShakeSearch.Controller.showSnackBar = (msg, dismissAfter = 3000) => {
   const snkBar = ShakeSearch.UI.get("#snackbar");
   requestAnimationFrame(() => {
     snkBar.innerText = msg;
     snkBar.classList.add("show");
   });
 
-  // Hide snackbar after 3 secs
   setTimeout(() => {
     requestAnimationFrame(() => {
       snkBar.classList.remove("show");
     });
-  }, 3000);
+  }, dismissAfter);
 };
 
 ShakeSearch.Controller.signalSearchStarted = () => {
@@ -68,58 +161,6 @@ ShakeSearch.Controller.signalSearchEnded = () => {
   });
 
   ShakeSearch.State.searching = false;
-};
-
-ShakeSearch.Controller.search = async (evt) => {
-  evt.preventDefault();
-  if (ShakeSearch.State.searching) return;
-
-  const form = evt.target;
-  const fData = new FormData(form);
-  const query = fData.get("query").trim();
-
-  if (!validationPtrn.test(query)) return;
-  if (ShakeSearch.State.prevQry && ShakeSearch.State.prevQry == query) return;
-
-  let results;
-  ShakeSearch.State.prevQry = query;
-  ShakeSearch.Controller.signalSearchStarted();
-  try {
-    const response = await fetch(`/search?q=${query}`);
-    results = await response.json();
-  } catch (err) {
-    ShakeSearch.Controller.showSnackBar("Search failed. Pls try again!");
-    console.warn(err.message);
-  } finally {
-    ShakeSearch.Controller.signalSearchEnded();
-  }
-
-  if (results) {
-    // TODO expand on this check if needed
-    ShakeSearch.Controller.displayResults(results);
-  }
-};
-
-// TODO move away from displaying the results with a table
-ShakeSearch.Controller.displayResults = (results) => {
-  const rows = results.reduce((trs, txt) => {
-    const tr = document.createElement("tr");
-    // TODO If needed, sanitize txt before displaying it
-    // Dev tools shows it has some special chars (\r\n e.t.c)
-    tr.innerText = txt;
-    trs.push(tr);
-    return trs;
-  }, []);
-
-  const tBody = document.querySelector("#table-body");
-  requestAnimationFrame(() => {
-    while (tBody.firstChild) {
-      tBody.removeChild(tBody.firstChild);
-    }
-    requestAnimationFrame(() => {
-      tBody.append(...rows);
-    });
-  });
 };
 
 ShakeSearch.Controller.startApp = () => {
