e20dde80e7b768f4b63783721332983415a68132
Charles Odili <chaluwa@gmail.com>
2023-03-03 02:58:07 -0700

    tiny cleanups

--- main.go
+++ main.go
@@ -194,7 +194,7 @@ func (s *Searcher) Search(query Query) Result {
 		count = len(matches)
 		for _, pos := range matches {
 			data = append(data, Match{
-				Phrase: s.data[pos[0]-85 : pos[1]+85],
+				Phrase: s.data[pos[0]-150 : pos[1]+150],
 			})
 		}
 		data = data[start:end]
